45e3fbe407a167bfb0381b464e3350ecb6113a56
Vitoria Carlosso <84900172049@senacrs.edu.br>
2025-05-12 10:15:41 -0300

    agora o kanye fala

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 let quotes = [];
 
-async function loadQuotes() {
-  const url = "https://raw.githubusercontent.com/ajzbc/kanye.rest/master/src/quotes.json";
-  const res = await fetch(url);
-  quotes = await res.json();
-}
+  async function loadQuotes() {
+    const url = "https://raw.githubusercontent.com/ajzbc/kanye.rest/master/src/quotes.json";
+    const res = await fetch(url);
+    quotes = await res.json();
+  }
 
-function getQuote() {
-  const quoteEl = document.getElementById("quote");
-  quoteEl.classList.add("fade-out");
-  setTimeout(() => {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    quoteEl.innerText = quotes[randomIndex];
-    quoteEl.classList.remove("fade-out");
-  }, 500);
-}
+  function getQuote() {
+    const quoteEl = document.getElementById("quote");
 
-loadQuotes().then(() => {
-  getQuote();
-});
+
+    quoteEl.classList.add("fade-out");
+
+    setTimeout(() => {
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      const newQuote = quotes[randomIndex];
+      quoteEl.innerText = newQuote;
+      quoteEl.classList.remove("fade-out");
+      speakText(newQuote);
+    }, 500);
+  }
+
+  function speakText(text) {
+    window.speechSynthesis.cancel();
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'en-US';
+    utterance.rate = 1;
+    utterance.pitch = 1;
+    utterance.volume = 1;
+    window.speechSynthesis.speak(utterance);
+  }
+  loadQuotes().then(() => {
+    getQuote();
+  });
